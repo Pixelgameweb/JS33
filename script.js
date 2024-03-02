1cfef0982365b62a142e2f8d5a966fe8cfe6e60c
||We(r)K3|R|| <122714529+Pixelgameweb@users.noreply.github.com>
2024-03-02 16:10:36 +0200

    Add files

---
+++ script.js
@@ -0,0 +1,43 @@
+// 1 задание
+// let city_li = document.querySelectorAll('li');
+// let city_es = []; 
+// city_li.forEach(city_li => {city_es.push(city_li.textContent)});
+// let json = JSON.stringify({ city_es });
+// console.log(json);
+
+// 2 задание
+// let json = '["user1","user2","user3","user4","user5"]'; 
+// let JSjson = JSON.parse(json);
+// JSjson.push("user6");
+// console.log(JSjson)
+
+//3 задание
+// let json = `[     
+// 	{
+// 		"name": "user1",
+// 		"age": 25,
+// 		"salary": 1000
+// 	},
+// 	{
+// 		"name": "user2",
+// 		"age": 26,
+// 		"salary": 2000
+// 	},
+// 	{
+// 		"name": "user3",
+// 		"age": 27,
+// 		"salary": 3000
+// 	}
+//     {
+// 		"name": "user4",
+// 		"age": 28,
+// 		"salary": 4000
+// 	}
+// ]`;
+
+// 4 задание
+
+// let json = '["user1","user2","user3","user4","user5"]'; 
+// let user = JSON.parse(json);
+// document.write('<ul>');
+// user.forEach(user => {document.write(`<li>${user}</li>`)});
